1d6fecce2081c36f4eee1c744b6ae29408c9563d
bahrikutlu <bahrikutlu@gmail.com>
2020-05-13 23:49:01 -0500

    turned off debug

--- Vidly_V2/settings.py
+++ Vidly_V2/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +124,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
